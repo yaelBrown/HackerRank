ce2bd5d986880354f69b6aaba80ffbad9e2f4733
Yael Brown <yaelrbrown@gmail.com>
2019-10-19 21:26:01 -0500

    Completed endOfFile exercise and started InitializerBlock.

---
+++ Java/src/InitializerBlock.java
@@ -0,0 +1,7 @@
+public class InitializerBlock {
+    // https://www.hackerrank.com/challenges/java-static-initializer-block/problem?h_r=next-challenge&h_v=zen
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
